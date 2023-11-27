61df6eb3854b42c68f13a1532024c88069940d74
Freegrattis <gabriel_fregatti@estudante.sesisenai.org.br>
2023-11-27 14:56:43 -0300

    update
    Inicio do trabalho de SA

--- SA - Calculadora de pagamento/Projeto Calculadora Salarial/Calculadora Salarial/src/EntradaSaida.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class EntradaSaida {
-
-    public static int menuLogin(int opcao) {
-        return Integer.parseInt(JOptionPane.showInputDialog("MENU\n[1]-Entrar como usuario\n[2]-Acessar painel de administrador\n[3]-Cadastrar novo usuario"));
-    }
-    
-}
